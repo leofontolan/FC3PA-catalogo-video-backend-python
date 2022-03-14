ca630c81a41f97c15c970a1d98b1dec5092878ac
leofontolan <leofontolan@gmail.com>
2022-03-14 19:57:59 -0300

    Melhorando estrutura de pastas do Unit Test

--- src/category/domain/entities.py
+++ src/category/domain/entities.py
@@ -11,10 +11,3 @@ class Category:
     description: Optional[str] = None
     is_active: Optional[bool] = True
     created_at: Optional[datetime] = field(default_factory=lambda: datetime.now())
-
-
-print(Category('Movie'))
-print(Category('Movie'))
-print(Category('Movie'))
-print(Category('Movie'))
-print(Category('Movie'))
